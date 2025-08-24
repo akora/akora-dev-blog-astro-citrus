04c5e03e8ebbe7ed51f115a7cdb561558c88e348
Andras Kora <916113+akora@users.noreply.github.com>
2025-08-24 09:12:05 +0200

    fix: add type definitions for environment variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,17 @@
+/// <reference types="astro/client" />
+
 declare module "@pagefind/default-ui" {
 	declare class PagefindUI {
 		constructor(arg: unknown);
 	}
+}
+
+interface ImportMetaEnv {
+	readonly WEBMENTION_API_KEY: string;
+	readonly WEBMENTION_URL: string;
+	readonly WEBMENTION_PINGBACK: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
 }
